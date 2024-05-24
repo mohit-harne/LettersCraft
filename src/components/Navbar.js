f569eae26a96bd34267374e1eb132161b60fc06e
mohit-harne <mohitharne@gmail.com>
2024-05-24 20:22:49 +0530

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,18 +29,22 @@ const Navbar = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <div className={`w-full bg-[#101D2E] z-50 ${isSticky ? 'fixed top-0 bg-opacity-75 [text-shadow:_0_1px_1px_rgb(0_0_0_/_100%)]' : 'sticky bg-opacity-5 top-0'}`}>
       <nav className="container mx-auto h-20 flex justify-between items-center lg:px-[180px] px-[20px]">
         <Link to="/" className="w-[105px] h-[32px]"><img className='drop-shadow-md' src={logo} alt='M' /></Link>
         <div className="hidden lg:flex grid-flow-col items-center gap-[24px]">
-          <CustomNavLink to="/" active={location.pathname === "/"}>Home</CustomNavLink>
-          <CustomNavLink to="/about" active={location.pathname === "/about"}>About</CustomNavLink>
-          <CustomNavLink to="/services" active={location.pathname === "/services"}>Services</CustomNavLink>
-          <CustomNavLink to="/languages" active={location.pathname === "/languages"}>Languages</CustomNavLink>
-          <CustomNavLink to="/epayments" active={location.pathname === "/epayments"}>ePayments</CustomNavLink>
-          <CustomNavLink to="/blog" active={location.pathname === "/blog"}>Blog</CustomNavLink>
-          <CustomNavLink to="/contact" active={location.pathname === "/contact"}>Contact</CustomNavLink>
+          <CustomNavLink to="/" active={location.pathname === "/"} onClick={closeMenu}>Home</CustomNavLink>
+          <CustomNavLink to="/about" active={location.pathname === "/about"} onClick={closeMenu}>About</CustomNavLink>
+          <CustomNavLink to="/services" active={location.pathname === "/services"} onClick={closeMenu}>Services</CustomNavLink>
+          <CustomNavLink to="/languages" active={location.pathname === "/languages"} onClick={closeMenu}>Languages</CustomNavLink>
+          <CustomNavLink to="/epayments" active={location.pathname === "/epayments"} onClick={closeMenu}>ePayments</CustomNavLink>
+          <CustomNavLink to="/blog" active={location.pathname === "/blog"} onClick={closeMenu}>Blog</CustomNavLink>
+          <CustomNavLink to="/contact" active={location.pathname === "/contact"} onClick={closeMenu}>Contact</CustomNavLink>
           <div className='flex gap-[8px]'>
             <Link to="/mail" className="bg-[#3D65FF] text-white text-[14px] font-medium py-[8px] px-[16px] rounded-[100px]">
               <div className='flex justify-center items-center gap-[4px]'>
@@ -70,13 +74,13 @@ const Navbar = () => {
       </nav>
       <div className={`lg:hidden absolute top-[80px] left-0 right-0 bg-[#101D2E] z-50 transition-all duration-300 ${isMenuOpen ? 'bg-opacity-75' : 'opacity-0 pointer-events-none'}`}>
         <div className="flex flex-col items-center py-4 lg:gap-0 gap-4 justify-center">
-          <CustomNavLink to="/" active={location.pathname === "/"}><h1 className='text-2xl font-bold'>Home</h1></CustomNavLink>
-          <CustomNavLink to="/about" active={location.pathname === "/about"}><h1 className='text-2xl font-bold'>About</h1></CustomNavLink>
-          <CustomNavLink to="/services" active={location.pathname === "/services"}><h1 className='text-2xl font-bold'>Services</h1></CustomNavLink>
-          <CustomNavLink to="/languages" active={location.pathname === "/languages"}><h1 className='text-2xl font-bold'>Languages</h1></CustomNavLink>
-          <CustomNavLink to="/epayments" active={location.pathname === "/epayments"}><h1 className='text-2xl font-bold'>ePayments</h1></CustomNavLink>
-          <CustomNavLink to="/blog" active={location.pathname === "/blog"}><h1 className='text-2xl font-bold'>Blog</h1></CustomNavLink>
-          <CustomNavLink to="/contact" active={location.pathname === "/contact"}><h1 className='text-2xl font-bold'>Contact</h1></CustomNavLink>
+          <CustomNavLink to="/" active={location.pathname === "/"} onClick={closeMenu}><h1 className='text-2xl font-bold'>Home</h1></CustomNavLink>
+          <CustomNavLink to="/about" active={location.pathname === "/about"} onClick={closeMenu}><h1 className='text-2xl font-bold'>About</h1></CustomNavLink>
+          <CustomNavLink to="/services" active={location.pathname === "/services"} onClick={closeMenu}><h1 className='text-2xl font-bold'>Services</h1></CustomNavLink>
+          <CustomNavLink to="/languages" active={location.pathname === "/languages"} onClick={closeMenu}><h1 className='text-2xl font-bold'>Languages</h1></CustomNavLink>
+          <CustomNavLink to="/epayments" active={location.pathname === "/epayments"} onClick={closeMenu}><h1 className='text-2xl font-bold'>ePayments</h1></CustomNavLink>
+          <CustomNavLink to="/blog" active={location.pathname === "/blog"} onClick={closeMenu}><h1 className='text-2xl font-bold'>Blog</h1></CustomNavLink>
+          <CustomNavLink to="/contact" active={location.pathname === "/contact"} onClick={closeMenu}><h1 className='text-2xl font-bold'>Contact</h1></CustomNavLink>
           <div className='flex gap-[8px]'>
             <Link to="/mail" className="bg-[#3D65FF] text-white text-[14px] font-medium py-[8px] px-[16px] rounded-[100px]">
               <div className='flex justify-center items-center gap-[4px]'>
@@ -98,9 +102,10 @@ const Navbar = () => {
 };
 
 // CustomNavLink component to handle active state
-const CustomNavLink = ({ to, active, children }) => (
+const CustomNavLink = ({ to, active, onClick, children }) => (
   <NavLink
     to={to}
+    onClick={onClick}
     className={({ isActive }) => `text-[14px] font-normal leading-[20.27px] ${isActive ? "text-gray-400" : "text-white"}`}
   >
     {children}
